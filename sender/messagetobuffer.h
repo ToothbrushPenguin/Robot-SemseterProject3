3ef7352280fe64d1adc2e84742002984dc2aa4bc
Emil K. Mathiesen <emil.k.m@hotmail.dk>
2021-09-18 21:25:42 +0200

    faster

--- sender/messagetobuffer.h
+++ sender/messagetobuffer.h
@@ -17,6 +17,16 @@ public:
     sf::SoundBuffer buffer;
     vector<sf::Int16> samplesAll;
 
+    vector<sf::Int16> a;
+    vector<sf::Int16> b;
+    vector<sf::Int16> c;
+    vector<sf::Int16> d;
+
+    vector<sf::Int16> e;
+    vector<sf::Int16> f;
+    vector<sf::Int16> g;
+    vector<sf::Int16> h;
+
     unsigned int samplerate = 8000;
     int milisecondsPrBip = 100;
     int packetLength = milisecondsPrBip * samplerate/1000;
