60499a1b8838b7b8909fb4bc65bd4a5f86ea27d0
Kristian Gjersen <kristian@kristian.G>
2021-10-05 15:43:53 +0200

    Attempted Fix

--- robotMovement/main.cpp
+++ robotMovement/main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 #include "robotmovement.h"
-#include "robotmovement.cpp"
 
 using namespace std;
 
+
+//---------------------------------
+
+// denne kodes placering skal måske ændres
+
+// Declared globally as signal handler can't be part of the class..
+mqtt::async_client* cli;
+mqtt::topic* top;
+mqtt::token_ptr tok;
+
+// Handle CTRL + C and stop the robot
+void signal_Handler(int signum) {
+    json stop_msg = {{"linear", {{"x", 0.0}, {"y", 0}, {"z", 0}}},
+    {"angular", {{"x", 0}, {"y", 0}, {"z", 0.0}}}
+    };
+    cout << "CTRL + C pressed, exiting.." << endl;
+    tok = top->publish(stop_msg.dump());
+    tok->wait();
+    exit(signum);
+};
+//----------------------------------
+
 int main()
 {
-    signal(SIGINT, signalHandler);
+    signal(SIGINT, signal_Handler);
     cout << "Initializing for server '" << ADDRESS << "'..." << endl;
     mqtt::async_client cli(ADDRESS, "");
     mqtt::topic top(cli, TOPIC, QOS);
@@ -20,6 +41,4 @@ int main()
     json move = ex.RobotMovement::Move(forward,speed);
 
     ex.publishMessage(move);
-
-
 }
--- robotMovement/robotmovement.h
+++ robotMovement/robotmovement.h
@@ -17,21 +17,8 @@ const string ADDRESS { "tcp://localhost:1883" };
 const string TOPIC { "cmd_vel" };
 const int QOS = 1;
 
-// Declared globally as signal handler can't be part of the class..
-mqtt::async_client* cli;
-mqtt::topic* top;
-mqtt::token_ptr tok;
 
-// Handle CTRL + C and stop the robot
-void signalHandler(int signum) {
-    json stop_msg = {{"linear", {{"x", 0.0}, {"y", 0}, {"z", 0}}},
-    {"angular", {{"x", 0}, {"y", 0}, {"z", 0.0}}}
-    };
-    cout << "CTRL + C pressed, exiting.." << endl;
-    tok = top->publish(stop_msg.dump());
-    tok->wait();
-    exit(signum);
-}
+
 //---------------------------------------------------------------------------------------//
 
 class RobotMovement
@@ -51,6 +38,13 @@ private:
     bool conn = false;
     double speed;
     double turnDir;
+//-------------------------------
+    mqtt::async_client* cli;
+    mqtt::topic* top;
+    mqtt::token_ptr tok;
+//-------------------------------
 };
+
+
 
 #endif // ROBOTMOVEMENT_H
